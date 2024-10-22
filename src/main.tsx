98fea914d1fa7a35d9e3ec649889f4da9bba270a
Ruben Dougall <Ruben9922@users.noreply.github.com>
2024-10-22 18:34:16 +0100

    Add Google Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import {ChakraProvider, ColorModeScript} from "@chakra-ui/react";
 import {StrictMode} from "react";
 import {createRoot} from "react-dom/client";
+import ReactGA from "react-ga4";
 import App from "./App.tsx";
 import theme from "./theme.ts";
 import "@fontsource/lato/400.css";
 import "@fontsource/lato/700.css";
+
+ReactGA.initialize("G-06YH40HMX8");
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
